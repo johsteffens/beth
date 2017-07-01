ced1dfdea33b3a6688a7472ed13f270b5f0a57fa
Johannes Steffens <johannes.b.steffens@gmail.com>
2017-07-01 17:24:19 +0200

    implemented a flavor of cuckoo hashing

---
+++ lib/bcore/bcore_hmap.c
@@ -0,0 +1,444 @@
+/// Author & Copyright (C) 2017 Johannes Steffens <johannes.b.steffens@gmail.com>. All rights reserved.
+
+#include "bcore_hmap.h"
+
+#include <time.h> // only for selftest
+
+
+typedef struct node_u2vd_s
+{
+    u2_t key;
+    unsigned trace_flag : 1;
+    vd_t val;
+} node_u2vd_s;
+
+typedef struct bcore_hmap_u2vd_s
+{
+    node_u2vd_s* data;
+    sz_t size;
+    sz_t space;
+    sz_t depth_limit;
+    sz_t size_limit;
+    bcore_hash_u2u2 h1;
+    bcore_hash_u2u2 h2;
+    bcore_hash_u2u2 h3;
+} bcore_hmap_u2vd_s;
+
+static u2_t hash_u2u2_1( u2_t key )
+{
+    u2_t h = ( 632432329 ^ ( ( key       ) & 0x0FFFFu ) ) * 88888888901;
+         h = ( h         ^ ( ( key >> 16 ) & 0x0FFFFu ) ) * 88888888901;
+    return h;
+}
+
+static u2_t hash_u2u2_2( u2_t key )
+{
+    u2_t h = ( 368653234 ^ ( ( key       ) & 0x0FFFFu ) ) * 77777777827;
+         h = ( h         ^ ( ( key >> 16 ) & 0x0FFFFu ) ) * 77777777827;
+    return h;
+}
+
+void bcore_hmap_u2vd_s_init( bcore_hmap_u2vd_s* o )
+{
+    bcore_memzero( o, sizeof( *o ) );
+    o->size_limit = 0xFFFFFFFFu;
+    o->h1         = hash_u2u2_1;
+    o->h2         = hash_u2u2_2;
+    o->h3         = NULL;
+}
+
+void bcore_hmap_u2vd_s_down( bcore_hmap_u2vd_s* o )
+{
+    bcore_un_alloc( sizeof( node_u2vd_s ), o->data, o->space, 0, NULL );
+}
+
+void bcore_hmap_u2vd_s_copy( bcore_hmap_u2vd_s* o, const bcore_hmap_u2vd_s* src )
+{
+    if( src->size > o->space )
+    {
+        o->data = bcore_un_alloc( sizeof( node_u2vd_s ), o->data, o->space,  0, &o->space );
+        o->data = bcore_un_alloc( sizeof( node_u2vd_s ), o->data, 0, src->size, &o->space );
+    }
+    bcore_memcpy( o->data, src->data, sizeof( node_u2vd_s ) * src->size );
+    o->size = src->size;
+}
+
+static void set_size( bcore_hmap_u2vd_s* o, sz_t size )
+{
+    if( ( size & ( size - 1 ) ) != 0 ) ERR( "size %zu must be a power of two", size );
+    if( size > o->space )
+    {
+        o->data = bcore_un_alloc( sizeof( node_u2vd_s ), o->data, o->space, 0, &o->space );
+        o->data = bcore_un_alloc( sizeof( node_u2vd_s ), o->data, 0, size,  &o->space );
+    }
+    o->size = size;
+    bcore_memzero( o->data, sizeof( node_u2vd_s ) * o->size );
+
+    // we set depth_limit to log2( size ) + 1;
+    o->depth_limit = 1;
+    while( size >> o->depth_limit ) o->depth_limit++;
+}
+
+DEFINE_FUNCTION_CREATE(  bcore_hmap_u2vd_s )
+DEFINE_FUNCTION_DISCARD( bcore_hmap_u2vd_s )
+DEFINE_FUNCTION_CLONE(   bcore_hmap_u2vd_s )
+
+/**********************************************************************************************************************/
+
+static sz_t find( const bcore_hmap_u2vd_s* o, u2_t key ) // returns valid index or o->size
+{
+    if( o->size == 0 ) return o->size;
+    u2_t mask = o->size - 1;
+    if( o->h1 )
+    {
+        sz_t idx = o->h1( key ) & mask;
+        if( o->data[ idx ].key == key ) return idx;
+    }
+
+    if( o->h2 )
+    {
+        sz_t idx = o->h2( key ) & mask;
+        if( o->data[ idx ].key == key ) return idx;
+    }
+
+    if( o->h3 )
+    {
+        sz_t idx = o->h3( key ) & mask;
+        if( o->data[ idx ].key == key ) return idx;
+    }
+
+    return o->size;
+}
+
+static node_u2vd_s* set( const bcore_hmap_u2vd_s* o, node_u2vd_s node, sz_t depth ) // sets new node
+{
+    if( o->size == 0 ) return NULL;
+    u2_t mask = o->size - 1;
+    node.trace_flag = 0;
+    if( depth == o->depth_limit ) return NULL;
+
+    node_u2vd_s* node1 = NULL;
+    if( o->h1 )
+    {
+        node1 = &o->data[ o->h1( node.key ) & mask ];
+        if( node1->key == 0 )
+        {
+            *node1 = node;
+            return node1;
+        }
+    }
+
+    node_u2vd_s* node2 = NULL;
+    if( o->h2 )
+    {
+        node2 = &o->data[ o->h2( node.key ) & mask ];
+        if( node2->key == 0 )
+        {
+            *node2 = node;
+            return node2;
+        }
+    }
+
+    node_u2vd_s* node3 = NULL;
+    if( o->h3 )
+    {
+        node3 = &o->data[ o->h3( node.key ) & mask ];
+        if( node3->key == 0 )
+        {
+            *node3 = node;
+            return node3;
+        }
+    }
+
+    if( node1 && !node1->trace_flag )
+    {
+        node1->trace_flag = 1;
+        if( set( o, *node1, depth + 1 ) )
+        {
+            *node1 = node;
+            return node1;
+        }
+        else
+        {
+            node1->trace_flag = 0;
+        }
+    }
+
+    if( node2 && !node2->trace_flag )
+    {
+        node2->trace_flag = 1;
+        if( set( o, *node2, depth + 1 ) )
+        {
+            *node2 = node;
+            return node2;
+        }
+        else
+        {
+            node2->trace_flag = 0;
+        }
+    }
+
+    if( node3 && !node3->trace_flag )
+    {
+        node3->trace_flag = 1;
+        if( set( o, *node3, depth + 1 ) )
+        {
+            *node3 = node;
+            return node3;
+        }
+        else
+        {
+            node3->trace_flag = 0;
+        }
+    }
+
+    return NULL;
+}
+
+void bcore_hmap_u2vd_s_set_hash( bcore_hmap_u2vd_s* o, sz_t index, bcore_hash_u2u2 hash )
+{
+    if( index > 2 ) ERR( "index (%zu) out of range", index );
+    switch( index )
+    {
+        case 0: o->h1 = hash; break;
+        case 1: o->h2 = hash; break;
+        case 2: o->h3 = hash; break;
+        default: break;
+    }
+}
+
+vd_t* bcore_hmap_u2vd_s_get( const bcore_hmap_u2vd_s* o, u2_t key )
+{
+    if( !key ) return NULL;
+    sz_t idx = find( o, key );
+    return ( idx < o->size ) ? &o->data[ idx ].val : NULL;
+}
+
+void bcore_hmap_u2vd_s_set( bcore_hmap_u2vd_s* o, u2_t key, vd_t val )
+{
+    if( !key ) ERR( "key is zero" );
+
+    {
+        sz_t idx = find( o, key );
+        if( idx < o->size )
+        {
+            o->data[ idx ].val = val;
+            return;
+        }
+    }
+
+    {
+        node_u2vd_s node;
+        node.key = key;
+        node.val = 0;
+        node_u2vd_s* p_node = set( o, node, 1 );
+        if( p_node )
+        {
+            p_node->val = val;
+            return;
+        }
+    }
+
+    // rehash
+    {
+        node_u2vd_s* buf_data  = o->data;
+        sz_t         buf_size  = o->size;
+        sz_t         buf_space = o->space;
+        if( o->size > 0 )
+        {
+            o->size *= 2;
+            o->depth_limit++;
+        }
+        else
+        {
+            o->size = 8;
+            o->depth_limit = 4;
+        }
+        if( o->size > o->size_limit ) ERR( "size limit (%zu) exceeded", o->size_limit );
+        o->data = bcore_u_alloc( sizeof( node_u2vd_s ), NULL, o->size, &o->space );
+        bcore_memzero( o->data, sizeof( node_u2vd_s ) * o->size );
+        for( sz_t i = 0; i < buf_size; i++ )
+        {
+            node_u2vd_s* p_node = &buf_data[ i ];
+            if( p_node->key ) bcore_hmap_u2vd_s_set( o, p_node->key, p_node->val );
+        }
+        bcore_un_alloc( sizeof( node_u2vd_s ), buf_data, buf_space, 0, NULL );
+    }
+
+    bcore_hmap_u2vd_s_set( o, key, val );
+}
+
+vd_t bcore_hmap_u2vd_s_remove( bcore_hmap_u2vd_s* o, u2_t key )
+{
+    if( !key ) return NULL;
+
+    sz_t idx = find( o, key );
+    if( idx < o->size )
+    {
+        node_u2vd_s* node = &o->data[ idx ];
+        vd_t ret = node->val;
+        node->key = 0;
+        node->val = 0;
+        return ret;
+    }
+    return NULL;
+}
+
+bool bcore_hmap_u2vd_s_exists( const bcore_hmap_u2vd_s* o, u2_t key )
+{
+    if( !key ) return false;
+    sz_t idx = find( o, key );
+    return ( idx < o->size );
+}
+
+void bcore_hmap_u2vd_s_clear( bcore_hmap_u2vd_s* o )
+{
+    set_size( o, 0 );
+}
+
+sz_t bcore_hmap_u2vd_s_keys( const bcore_hmap_u2vd_s* o )
+{
+    sz_t count = 0;
+    for( sz_t i = 0; i < o->size; i++ ) count += ( o->data[ i ].key > 0 );
+    return count;
+}
+
+sz_t bcore_hmap_u2vd_s_space( const bcore_hmap_u2vd_s* o )
+{
+    return o->size;
+}
+
+/**********************************************************************************************************************/
+
+bcore_string_s* bcore_hmap_u2vd_s_status( bcore_hmap_u2vd_s* o )
+{
+    bcore_string_s* string = bcore_string_s_create();
+    sz_t keys = bcore_hmap_u2vd_s_keys( o );
+    bcore_string_s_pushf( string, "keys ........... %zu\n", keys );
+    bcore_string_s_pushf( string, "nodes .......... %lu\n", o->size );
+    bcore_string_s_pushf( string, "keys/nodes ..... %5.4f\n", o->size > 0 ? ( f3_t )( keys ) / o->size : 0 );
+    return string;
+}
+
+void bcore_hmap_u2vd_filltest()
+{
+    bcore_life_s* life = bcore_life_s_create();
+    bcore_hmap_u2vd_s* map = bcore_life_s_push( life, bcore_hmap_u2vd_s_discard, bcore_hmap_u2vd_s_create() );
+
+    sz_t cycles = 2;
+    sz_t size = 65536 * 64;
+
+    for( sz_t i = 0; i < cycles; i++ )
+    {
+        bcore_hmap_u2vd_s_clear( map );
+        u2_t key = bcore_xsg3_u2( i + 1 );
+        for( sz_t j = 0; j < size; j++ )
+        {
+            key = bcore_xsg_u2( key );
+            sz_t size1 = map->size;
+            bcore_hmap_u2vd_s_set( map, key, 0 );
+            sz_t size2 = map->size;
+            if( size2 > size1 )
+            {
+                bcore_string_s_print_d( bcore_string_s_createf( "%zu/%zu (%f)\n", j, size1, size1 > 0 ? ( f3_t ) j / size1 : 0.0 ) );
+            }
+        }
+        bcore_string_s_print_d( bcore_hmap_u2vd_s_status( map ) );
+    }
+
+    bcore_life_s_discard( life );
+}
+
+bcore_string_s* bcore_hmap_u2vd_selftest()
+{
+    bcore_string_s* log = bcore_string_s_create();
+    bcore_hmap_u2vd_s* map = bcore_hmap_u2vd_s_create();
+    const sz_t cycles = 1000000;
+
+    typedef struct
+    {
+        u2_t key;
+        vd_t val;
+    } kv_s;
+
+    kv_s* kvbuf = bcore_alloc( NULL, cycles * sizeof( kv_s ) );
+    sz_t kvbuf_size = 0;
+
+    clock_t time = clock();
+    bcore_string_s_pushf( log, "Mixed access: " );
+
+    {
+        u3_t rval1 = 1;
+        u3_t rval2 = 12345;
+        for( sz_t i = 0; i < cycles; i++ )
+        {
+            rval1 = bcore_xsg_u2( rval1 );
+            rval2 = bcore_xsg_u2( rval2 );
+            kv_s kv;
+            kv.key = ( u2_t )rval1;
+            kv.val = ( vd_t )rval2;
+            kvbuf[ kvbuf_size++ ] = kv;
+
+            // set
+            bcore_hmap_u2vd_s_set( map, kv.key, kv.val );
+
+            // retrieve
+            rval1 = bcore_xsg_u2( rval1 );
+            kv = kvbuf[ rval1 % kvbuf_size ];
+            vd_t* val_ptr = bcore_hmap_u2vd_s_get( map, kv.key );
+            if( kv.val != *val_ptr ) ERR( "value mismatch (%lu vs %lu)", kv.val, *val_ptr );
+
+            // delete
+            rval1 = bcore_xsg_u2( rval1 );
+            if( ( ( rval1 >> 10 ) & 1 ) == 1 )
+            {
+                rval1 = bcore_xsg_u2( rval1 );
+                sz_t idx = rval1 % kvbuf_size;
+                kv_s kv = kvbuf[ idx ];
+                if( !bcore_hmap_u2vd_s_get( map, kv.key ) )  ERR( "key (%lu) not found", kv.key );
+                bcore_hmap_u2vd_s_remove( map, kv.key );
+                if( bcore_hmap_u2vd_s_get( map, kv.key ) )  ERR( "deleted key still exists (%lu)", kv.key );
+                kvbuf_size--;
+                kvbuf[ idx ] = kvbuf[ kvbuf_size ];
+            }
+        }
+    }
+
+    time = clock() - time;
+    bcore_string_s_pushf( log, "(%5.3fs)\n", ( double )time/CLOCKS_PER_SEC );
+
+    bcore_string_s_push_string_d( log, bcore_hmap_u2vd_s_status( map ) );
+
+    time = clock();
+    bcore_string_s_pushf( log, "\nRead-access of %lu keys: ", kvbuf_size );
+    sz_t read_cycles = 20;
+    for( sz_t j = 0; j < read_cycles; j++ )
+    {
+        for( sz_t i = 0; i < kvbuf_size; i++ )
+        {
+            if( *bcore_hmap_u2vd_s_get( map, kvbuf[ i ].key ) != kvbuf[ i ].val ) ERR( "value mismatch (%lu vs %lu)", kvbuf[ i ].key, kvbuf[ i ].val );
+        }
+    }
+    time = clock() - time;
+    bcore_string_s_pushf( log, "(%gs per access)\n", ( ( double )time/CLOCKS_PER_SEC ) / ( kvbuf_size * read_cycles ) );
+
+    bcore_string_s_pushf( log, "\n" );
+    bcore_string_s_pushf( log, "Removal: " );
+    time = clock();
+    while( kvbuf_size )
+    {
+        kvbuf_size--;
+        kv_s kv = kvbuf[ kvbuf_size ];
+        if( !bcore_hmap_u2vd_s_get( map, kv.key ) )  ERR( "key (%lu) not found", kv.key );
+        bcore_hmap_u2vd_s_remove( map, kv.key );
+        if( bcore_hmap_u2vd_s_get( map, kv.key ) )  ERR( "deleted key still exists (%lu)", kv.key );
+    }
+    time = clock() - time;
+    bcore_string_s_pushf( log, "(%5.3fs)\n", ( double )time/CLOCKS_PER_SEC );
+
+    bcore_string_s_push_string_d( log, bcore_hmap_u2vd_s_status( map ) );
+
+    bcore_hmap_u2vd_s_discard( map );
+    bcore_alloc( kvbuf, 0 );
+    return log;
+}
---
+++ lib/bcore/bcore_hmap.h
@@ -0,0 +1,55 @@
+/// Author & Copyright (C) 2017 Johannes Steffens <johannes.b.steffens@gmail.com>. All rights reserved.
+
+#ifndef BCORE_HMAP_H
+#define BCORE_HMAP_H
+
+#include "bcore_features.h"
+#include "bcore_string.h"
+
+/** Hashmap implementing a flavor of "cuckoo hashing" with single monolithic storage and
+ *  up to three hash functions (default: 2 functions).
+ *  Average storage efficiency is comparable to btree.
+ *
+ *  Advantages:
+ *     Complexity   : O(1) for insert, remove, retrieve.
+ *     Average speed: Typically one order of magnitude better than btree.
+ *
+ *  Disadvantage:
+ *     No well defined lower bound on worst case storage efficiency.
+ *     However, in practice a significant deviation from average efficiency is very unlikely.
+ *     (See studies on cuckoo hashing for more details).
+ */
+
+/**********************************************************************************************************************/
+
+/** bcore_hmap_u2vd_s:
+ *    key-type: u2_d
+ *    val-type: vd_t.
+ *    Maximum number of keys: 2^32
+ *    Map does not assume ownership of objects referenced by val.
+ */
+typedef struct bcore_hmap_u2vd_s bcore_hmap_u2vd_s;
+typedef u2_t (*bcore_hash_u2u2)( u2_t v );
+
+DECLARE_FUNCTION_INIT(    bcore_hmap_u2vd_s )
+DECLARE_FUNCTION_DOWN(    bcore_hmap_u2vd_s )
+DECLARE_FUNCTION_COPY(    bcore_hmap_u2vd_s )
+DECLARE_FUNCTION_CREATE(  bcore_hmap_u2vd_s )
+DECLARE_FUNCTION_DISCARD( bcore_hmap_u2vd_s )
+DECLARE_FUNCTION_CLONE(   bcore_hmap_u2vd_s )
+
+void  bcore_hmap_u2vd_s_set_hash(     bcore_hmap_u2vd_s* o, sz_t index, bcore_hash_u2u2 hash ); // sets optional external hash function (up to thee functions can be specified index 0, 1, 2)
+vd_t* bcore_hmap_u2vd_s_get(    const bcore_hmap_u2vd_s* o, u2_t key ); // returns pinter to value or NULL when key does not exist
+void  bcore_hmap_u2vd_s_set(          bcore_hmap_u2vd_s* o, u2_t key, vd_t val );
+vd_t  bcore_hmap_u2vd_s_remove(       bcore_hmap_u2vd_s* o, u2_t key ); // removes key and returns its value. returns NULL if key did not exist
+bool  bcore_hmap_u2vd_s_exists( const bcore_hmap_u2vd_s* o, u2_t key ); // checks if key exists
+void  bcore_hmap_u2vd_s_clear(        bcore_hmap_u2vd_s* o           ); // removes all entries and frees memory
+sz_t  bcore_hmap_u2vd_s_keys(   const bcore_hmap_u2vd_s* o           ); // returns number of registered keys
+sz_t  bcore_hmap_u2vd_s_size(   const bcore_hmap_u2vd_s* o           ); // returns current size of the hash map
+
+bcore_string_s* bcore_hmap_u2vd_selftest();
+
+/**********************************************************************************************************************/
+
+#endif // BCORE_HMAP_H
+
