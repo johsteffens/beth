5f14cb7e132b431f96a8ca88f74e5ac883eae68e
Johannes Steffens <johannes.b.steffens@gmail.com>
2017-09-12 16:17:38 +0200

    worked on trait framework

--- lib/bcore/bcore_trait.h
+++ lib/bcore/bcore_trait.h
@@ -16,10 +16,14 @@
  *  A trait instance is associated with its identifier via tp->vd map
  */
 
+/// Support feature: Tests if object supports trait. In case of non-support a log != NULL may be filled with descriptive reasons
+typedef bl_t ( *bcore_fp_supports )( tp_t object, tp_t trait, bcore_string_s* log );
+
 /// associates requirements with a trait
-/// this can only be done on new (not yet defined) traits
+/// this must be finished before the trait is defined below
 bl_t bcore_trait_require_function(  tp_t trait, tp_t function, tp_t name );
 bl_t bcore_trait_require_awareness( tp_t trait );
+bl_t bcore_trait_register_fp_support( tp_t trait, bcore_fp_supports f );
 
 /// defines a trait; parent may be 0; reentrant; thread-safe
 void bcore_trait_define( tp_t trait, tp_t parent );
@@ -27,11 +31,14 @@ void bcore_trait_define( tp_t trait, tp_t parent );
 /// trait existence; thread-safe
 bl_t bcore_trait_exists( tp_t trait );
 
-/// returns immediate parent of a trait; 0 if trait does not exist; thread-safe
+/// returns direct parent of a trait; 0 if trait does not exist; thread-safe
 tp_t bcore_trait_parent( tp_t trait );
 
 /// checks if ancestor is in the chain of inheritance of trait; thread-safe
 bl_t bcore_trait_is( tp_t trait, tp_t ancestor );
+
+/// checks if an object supports a trait
+bl_t bcore_trait_supports( tp_t object, tp_t trait, bcore_string_s* log );
 
 vd_t bcore_trait_signal( tp_t target, tp_t signal, vd_t object );
 
