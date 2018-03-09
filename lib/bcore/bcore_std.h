956dfd924b8e6908b395bf4c83ca5e597c0cda3d
Johannes Steffens <johannes.b.steffens@gmail.com>
2018-03-09 13:32:54 +0100

    added wrapping header with most used components

---
+++ lib/bcore/bcore_std.h
@@ -0,0 +1,37 @@
+/** List of most frequently used headers by dependent code */
+
+/** Copyright 2018 Johannes Bernhard Steffens
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef BCORE_STD_H
+#define BCORE_STD_H
+
+#include "bcore.h"
+#include "bcore_signal.h"
+#include "bcore_features.h"
+#include "bcore_flect.h"
+#include "bcore_trait.h"
+#include "bcore_spect_inst.h"
+#include "bcore_spect_array.h"
+#include "bcore_spect_source.h"
+#include "bcore_spect_sink.h"
+#include "bcore_txt_ml.h"
+#include "bcore_bin_ml.h"
+#include "bcore_quicktypes.h"
+#include "bcore_life.h"
+#include "bcore_sinks.h"
+#include "bcore_sources.h"
+
+#endif // BCORE_STD_H
