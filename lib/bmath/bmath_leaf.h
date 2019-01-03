8d6387de8c0be3040fcec11aa1e0d1bdd695117c
Johannes Steffens <johannes.b.steffens@gmail.com>
2019-01-03 17:18:11 +0100

    added NAN test

--- lib/bmath/bmath_leaf.h
+++ lib/bmath/bmath_leaf.h
@@ -39,6 +39,8 @@ static inline f3_t f3_pi( void )   { return 3.1415926535897932384626434; } // PI
 static inline f3_t f3_pi_2( void ) { return 1.5707963267948966192313217; } // PI / 2
 static inline f3_t f3_tau( void )  { return 6.2831853071795864769252868; } // 2 * PI
 
+static inline bl_t f3_is_nan( f3_t v ) { return v != v; } // nan compares unequal to itself
+
 /// random generator (range -1, 1)
 static inline f3_t f3_xsg1_sym( u2_t* rv ) { return ( *rv = bcore_xsg1_u2( *rv ) ) * ( 2.0 / 0xFFFFFFFFu ) - 1.0; }
 static inline f3_t f3_xsg2_sym( u2_t* rv ) { return ( *rv = bcore_xsg2_u2( *rv ) ) * ( 2.0 / 0xFFFFFFFFu ) - 1.0; } // do not use for polar coords
@@ -48,6 +50,7 @@ static inline f3_t f3_rnd_sym(  u2_t* rv ) { return ( *rv = bcore_xsg1_u2( *rv )
 static inline f3_t f3_xsg1_pos( u2_t* rv ) { return ( *rv = bcore_xsg1_u2( *rv ) ) * ( 1.0 / 0xFFFFFFFFu ); }
 static inline f3_t f3_xsg2_pos( u2_t* rv ) { return ( *rv = bcore_xsg2_u2( *rv ) ) * ( 1.0 / 0xFFFFFFFFu ); } // do not use for polar coords
 static inline f3_t f3_rnd_pos(  u2_t* rv ) { return ( *rv = bcore_xsg1_u2( *rv ) ) * ( 1.0 / 0xFFFFFFFFu ); }
+
 
 u2_t bmath_u2_argb_from_f3( vd_t, f3_t v ); // fist argument ignored (can be NULL)
 
