b363d7787785b6f39a346f9c745801d8816e5664
johannes <johannes.b.steffens@gmail.com>
2019-10-23 13:07:02 +0200

    Copyright -> Author and Copyright

--- lib/badapt/badapt_std.h
+++ lib/badapt/badapt_std.h
@@ -1,6 +1,6 @@
 /** List of most frequently used headers by dependent code */
 
-/** Copyright 2019 Johannes Bernhard Steffens
+/** Author and Copyright 2019 Johannes Bernhard Steffens
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
