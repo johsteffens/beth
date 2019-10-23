1158e1f52521b75b99d56bf6948b6a6da6491432
johannes <johannes.b.steffens@gmail.com>
2019-10-23 13:06:04 +0200

    Copyright -> Author and Copyright

--- lib/bcore/bcore_spect_inst_call.c
+++ lib/bcore/bcore_spect_inst_call.c
@@ -1,4 +1,4 @@
-/** Copyright 2019 Johannes Bernhard Steffens
+/** Author and Copyright 2019 Johannes Bernhard Steffens
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
