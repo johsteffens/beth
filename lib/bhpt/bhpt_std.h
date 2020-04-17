f9b8daf768472694d2841c5559c709ae80750c27
Johannes Steffens <johannes.b.steffens@gmail.com>
2020-04-17 19:59:22 +0200

    corrected copyright year

--- lib/bhpt/bhpt_std.h
+++ lib/bhpt/bhpt_std.h
@@ -1,6 +1,6 @@
 /** List of most frequently used headers by dependent code */
 
-/** Author and Copyright 2019 Johannes Bernhard Steffens
+/** Author and Copyright 2020 Johannes Bernhard Steffens
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -20,5 +20,7 @@
 
 #include "bhvm_std.h"
 #include "bhpt_signal.h"
+#include "bhpt_planted.h"
+#include "bhpt_frame.h"
 
 #endif // BHPT_STD_H
