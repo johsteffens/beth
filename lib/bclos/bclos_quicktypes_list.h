357d540c468874be70abdab41dbe93ac9db26bb0
johannes <johannes.b.steffens@gmail.com>
2020-08-08 11:16:00 +0200

    removed xoila_out files (except in bcore) because these files can be recomputed using xoico

--- lib/bclos/bclos_quicktypes_list.h
+++ lib/bclos/bclos_quicktypes_list.h
@@ -2,19 +2,19 @@
  *  Created via bclos_quicktypes_to_stdout( NULL );
  */
 
-#define TYPEOF_bclos_address_s 0xA6DC7313FB4E2519ull
-#define TYPEOF_bclos_arg_sig_s 0x2C6D077A4F39C403ull
-#define TYPEOF_bclos_arguments_s 0x9C06438175F85005ull
-#define TYPEOF_bclos_closure 0x9A7425F9188AA286ull
-#define TYPEOF_bclos_closure_s 0x2BA5A9BD224C95D8ull
-#define TYPEOF_bclos_expression_s 0x239A5883737ACE21ull
-#define TYPEOF_bclos_frame_s 0x94BA112F148EC83Cull
-#define TYPEOF_bclos_identity_s 0x0243DB7A7BEC2A09ull
-#define TYPEOF_bclos_library_arr_s 0x6F77F06EBFED3EE6ull
-#define TYPEOF_bclos_library_s 0xE07832A51863C5DEull
+#define TYPEOF_bclos_address_s      0xA6DC7313FB4E2519ull
+#define TYPEOF_bclos_arg_sig_s      0x2C6D077A4F39C403ull
+#define TYPEOF_bclos_arguments_s    0x9C06438175F85005ull
+#define TYPEOF_bclos_closure        0x9A7425F9188AA286ull
+#define TYPEOF_bclos_closure_s      0x2BA5A9BD224C95D8ull
+#define TYPEOF_bclos_expression_s   0x239A5883737ACE21ull
+#define TYPEOF_bclos_frame_s        0x94BA112F148EC83Cull
+#define TYPEOF_bclos_identity_s     0x0243DB7A7BEC2A09ull
+#define TYPEOF_bclos_library_arr_s  0x6F77F06EBFED3EE6ull
+#define TYPEOF_bclos_library_s      0xE07832A51863C5DEull
 #define TYPEOF_bclos_proc_binding_s 0xF4B1873AC009FE05ull
-#define TYPEOF_bclos_procedure_s 0xB57BB2F8C62BBC24ull
-#define TYPEOF_bclos_return 0xA8179535089C178Bull
-#define TYPEOF_bclos_signature_s 0x68100454BA508A93ull
-#define TYPEOF_bclos_statement_s 0x8F890CC7FF7E49ACull
-#define TYPEOF_bclos_syntax_s 0xFA4CD8E37AED5290ull
+#define TYPEOF_bclos_procedure_s    0xB57BB2F8C62BBC24ull
+#define TYPEOF_bclos_return         0xA8179535089C178Bull
+#define TYPEOF_bclos_signature_s    0x68100454BA508A93ull
+#define TYPEOF_bclos_statement_s    0x8F890CC7FF7E49ACull
+#define TYPEOF_bclos_syntax_s       0xFA4CD8E37AED5290ull
