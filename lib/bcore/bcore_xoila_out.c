d8157a3198d136bcd2cfc361a16288f0ab8f2167
Johannes Steffens <johannes.b.steffens@gmail.com>
2020-08-15 15:34:16 +0200

    changed hash computation

--- lib/bcore/bcore_xoila_out.c
+++ lib/bcore/bcore_xoila_out.c
@@ -1,6 +1,6 @@
 /** This file was generated from xoila source code.
  *  Compiling Agent : xoico_compiler (C) 2020 J.B.Steffens
- *  Last File Update: 2020-08-14T12:43:10Z
+ *  Last File Update: 2020-08-15T13:27:46Z
  *
  *  Copyright and License of this File:
  *
@@ -1284,4 +1284,4 @@ vd_t bcore_xoila_out_signal_handler( const bcore_signal_s* o )
     }
     return NULL;
 }
-// XOILA_OUT_SIGNATURE 0xC80EEFCD15F8BDD4ull
+// XOILA_OUT_SIGNATURE 0xC0C486AFFB657393ull
