296e1c21152da2a3d8ce6cb7caf65cbc886b9679
johannes <johannes.b.steffens@gmail.com>
2020-08-27 11:58:26 +0200

    xoico: refactoring

--- lib/bcore/bcore_xoila_out.h
+++ lib/bcore/bcore_xoila_out.h
@@ -1,6 +1,6 @@
 /** This file was generated from xoila source code.
  *  Compiling Agent : xoico_compiler (C) 2020 J.B.Steffens
- *  Last File Update: 2020-08-27T09:32:45Z
+ *  Last File Update: 2020-08-27T09:57:27Z
  *
  *  Copyright and License of this File:
  *
@@ -25,7 +25,7 @@
 #include "bcore_control.h"
 
 //To force a rebuild of this target by xoico, reset the hash key value below to 0.
-#define HKEYOF_bcore_xoila_out 0xC2F27F7208A6BA29ull
+#define HKEYOF_bcore_xoila_out 0xC0A5BA32C48CAC47ull
 
 #define TYPEOF_bcore_xoila_out 0xC52DA50A6069BDB1ull
 
@@ -620,4 +620,4 @@
 vd_t bcore_xoila_out_signal_handler( const bcore_signal_s* o );
 
 #endif // BCORE_XOILA_OUT_H
-// XOILA_OUT_SIGNATURE 0x3097ED922009C0EDull
+// XOILA_OUT_SIGNATURE 0x3F68A6361B1A1FD2ull
