3e2327d196e2b2714d11e5c67bc6e10de9408f4f
johannes <johannes.b.steffens@gmail.com>
2021-02-10 12:07:29 +0100

    finalized reflection framework

--- lib/bcore/bcore_x_via.h
+++ lib/bcore/bcore_x_via.h
@@ -59,8 +59,6 @@ func (sr_s t_c_get_sr( tp_t t, c obliv @* o, tp_t name ));
 
 /// selects element in o and returns a reference to it; returns NULL on no-match and for shells.
 func (c x_inst* c_get( c aware @* o, tp_t name )) = { sr_s sr = o.c_get_sr( name ); if( sr.is_strong() ) sr.clear(); return sr.o; };
-
-/// selects element in o and returns a reference to it; returns NULL on no-match and for shells.
 func (m x_inst* m_get( m aware @* o, tp_t name )) = { sr_s sr = o.m_get_sr( name ); if( sr.is_strong() ) sr.clear(); return sr.o; };
 
 /// sets element in o and returns a smart reference to it; returns sr_null() for shells.
@@ -69,9 +67,14 @@ func (sr_s t_set_sr( tp_t t, m obliv @* o, tp_t name, sr_s sr_src ));
 
 /// sets element in o and returns a reference to it; returns NULL for shells.
 func (m x_inst* set_c( m aware @* o, tp_t name, c x_inst* src )) = { return o.set_sr( name, sr_awc( src ) ).o; };
-
-/// sets element in o and returns a reference to it; returns NULL for shells.
 func (m x_inst* set_d( m aware @* o, tp_t name, d x_inst* src )) = { return o.set_sr( name, sr_asd( src ) ).o; };
+
+//----------------------------------------------------------------------------------------------------------------------
+
+/**********************************************************************************************************************/
+/** Path specifies an array of types addressing a specific sub-element in the object-tree.
+ *  The empty path is a valid address referring to the root object.
+ */
 
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -90,21 +93,59 @@ stamp :path_s = aware x_inst
         o.push( index );
     };
 
-    /// parses format: <name>.<name>. ....
-    /// parses format: <name>.[<index_literal>]. ...
-    func ( o parse(    m@* o, m bcore_source* source ));
-    func ( o parse_sc( m@* o, sc_t sc )) = { return o.parse( bcore_source_string_s_create_sc( sc )^ ); };
+    /// parses format: <name>.<name>. .... or <name>.[<index_literal>]. ...
+    func (o parse(    m@* o, m bcore_source* source ));
+    func (o parse_sc( m@* o, sc_t sc )) = { return o.parse( bcore_source_string_s_create_sc( sc )^ ); };
+
+    func (o to_sink( c@* o, m bcore_sink* sink ));
+
+    /// checks if given name exists
+    func (bl_t exists_in  ( c@* o,         c aware x_inst* inst )) = { return o.exists_in_t( inst._, inst ); };
+    func (bl_t exists_in_t( c@* o, tp_t t, c obliv x_inst* inst )) = { sr_s^ sr; sr = o.get_sr_in_t( t, inst ); return sr.o != NULL; };
+
+    /// returns type of element; returns 0 if not existing
+    func (tp_t type_in  ( c@* o,         c aware x_inst* inst )) = { return o.type_in_t( inst._, inst ); };
+    func (tp_t type_in_t( c@* o, tp_t t, c obliv x_inst* inst )) = { sr_s^ sr; sr = o.get_sr_in_t( t, inst ); return sr.type(); };
 
     /// returns sr_null in case path is not found in inst
     func (sr_s get_sr_in  ( c@* o,         c aware x_inst* inst )) = { return o.get_sr_in_t( inst._, inst ); };
     func (sr_s get_sr_in_t( c@* o, tp_t t, c obliv x_inst* inst ));
 
-    func (bl_t exists_in  ( c@* o,         c aware x_inst* inst )) = { return o.exists_in_t( inst._, inst ); };
-    func (bl_t exists_in_t( c@* o, tp_t t, c obliv x_inst* inst )) = { sr_s^ sr; return ( sr = o.get_sr_in_t( t, inst ) ).o != NULL; };
-
     /// returns NULL if not found or element is a shell
     func (c x_inst* c_get_in( c@* o, c obliv x_inst* inst )) = { sr_s sr = o.get_sr_in( inst ); if( sr.is_strong() ) sr.clear(); return sr.o.cast( c x_inst* ); };
     func (m x_inst* m_get_in( c@* o, m obliv x_inst* inst )) = { sr_s sr = o.get_sr_in( inst ); if( sr.is_strong() ) sr.clear(); return sr.o.cast( m x_inst* ); };
+
+    /// sets element in inst and returns a smart reference to it; returns sr_null() for invalid path or shells and does not change object.
+    func (sr_s set_sr_in  ( c@* o,         m aware x_inst* inst, sr_s sr_src )) = { return o.set_sr_in_t( inst._, inst, sr_src ); };
+    func (sr_s set_sr_in_t( c@* o, tp_t t, m obliv x_inst* inst, sr_s sr_src ));
+
+    /// sets element in o and returns a reference to it; returns NULL for shells.
+    func (m x_inst* set_c_in( c@* o, m aware x_inst* inst, c x_inst* src )) = { return o.set_sr_in( inst, sr_awc( src ) ).o; };
+    func (m x_inst* set_d_in( c@* o, m aware x_inst* inst, d x_inst* src )) = { return o.set_sr_in( inst, sr_asd( src ) ).o; };
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
+stamp :path_adl_s = aware x_array { :path_s => []; };
+
+//----------------------------------------------------------------------------------------------------------------------
+
+func (:path_s) to_sink =
+{
+    for( sz_t i = 0; i < o.size; i++ )
+    {
+        if( i > 0 ) sink.push_fa( ".", o.[ i ] );
+        if( o.[ i ] == TYPEOF_:path_s_array_index )
+        {
+            i++;
+            sink.push_fa( "[#<tp_t>]", o.[ i ] );
+        }
+        else
+        {
+            sink.push_fa( "#<sc_t>", bnameof( o.[ i ] ) );
+        }
+    }
+    return o;
 };
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -152,7 +193,9 @@ func (:path_s) get_sr_in_t =
     for( sz_t i = 0; i < o.size; i++ )
     {
         c bcore_via* via = cast( sr0.o, c bcore_via* );
-        p = p ? bcore_via_p_iget_via( p, via, index ) : bcore_via_s_get_typed( sr_s_type( &sr0 ) );
+        p = ( p && index >= 0 ) ? bcore_via_p_iget_via( p, via, index ) : NULL;
+        if( !p ) p = bcore_via_s_get_typed( sr_s_type( &sr0 ) );
+        if( !p ) { found = false; break; }
         sr_s sr1 = sr_null();
 
         if( o.[ i ] == TYPEOF_:path_s_array_index )
@@ -178,11 +221,70 @@ func (:path_s) get_sr_in_t =
         if( !sr0.o ) return sr_null();
     }
 
-    if( !found )
+    if( !found ) sr0.clear();
+
+    return sr0;
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
+/// returns sr_null in case path is not found in inst
+func (:path_s) set_sr_in_t =
+{
+    sr_s sr0 = sr_twc( t, inst );
+
+    sr_s^ src_hook;
+    src_hook = sr_src;
+    sr_src.set_strong( false );
+
+    if( !sr0.o ) return sr_null();
+
+    if( o.size == 0 && sr_src.o ) bcore_inst_x_copy_typed( sr0, sr_src.type(), sr_src.o );
+
+    const bcore_via_s* p = NULL;
+    sz_t index = -1;
+
+    bl_t found = true;
+
+    for( sz_t i = 0; i < o.size; i++ )
     {
+        m bcore_via* via = cast( sr0.o, m bcore_via* );
+        p = ( p && index >= 0 ) ? bcore_via_p_iget_via( p, via, index ) : NULL;
+        if( !p ) p = bcore_via_s_get_typed( sr_s_type( &sr0 ) );
+        if( !p ) { found = false; break; }
+
+        sr_s sr1 = sr_null();
+
+        if( o.[ i ] == TYPEOF_:path_s_array_index )
+        {
+            if( !bcore_spect_trait_supported( TYPEOF_bcore_array, sr_s_type( &sr0 ) ) ) { found = false; break; }
+            const bcore_array_s* p_array = bcore_array_s_get_typed( sr_s_type( &sr0 ) );
+            m bcore_array* array = via;
+            i++;
+            uz_t arr_index = o.[ i ];
+            uz_t arr_size = bcore_array_p_get_size( p_array, array );
+            if( arr_index >= arr_size ) { found = false; break; }
+
+            if( i + 1 == o.size ) bcore_array_p_set( p_array, array, arr_index, sr_src );
+
+            sr1 = bcore_array_p_get( p_array, array, arr_index );
+            p = NULL;
+        }
+        else
+        {
+            index = bcore_via_p_nget_index( p, via, o.[ i ] );
+            if( index < 0 ) { found = false; break; }
+
+            if( i + 1 == o.size ) bcore_via_p_iset( p, via, index, sr_src );
+
+            sr1 = bcore_via_p_iget( p, via, index );
+        }
         sr_down( sr0 );
-        sr0 = sr_null();
+        sr0 = sr1;
+        if( !sr0.o ) return sr_null();
     }
+
+    if( !found ) sr0.clear();
 
     return sr0;
 };
@@ -203,14 +305,18 @@ func :.t_m_get_sr =
 
 //----------------------------------------------------------------------------------------------------------------------
 
-/// sets element in o and returns a smart reference to it; returns sr_null() for shells.
+/// sets element in o and returns a smart reference to it; returns sr_null() for invalid address or shells.
 func :.t_set_sr =
 {
     const bcore_via_s* p = bcore_via_s_get_typed( t );
     sz_t index = bcore_via_p_nget_index( p, o.cast( c bcore_via* ), name );
-    if( index < 0 ) ERR_fa( "'#<sc_t>' has no element of name '#<sc_t>'", bifnameof( p->header.o_type ), bifnameof( name ) );
+    if( index < 0 )
+    {
+        sr_src.down();
+        return sr_null();
+    }
     bcore_via_p_iset( p, o.cast( m bcore_via* ), index, sr_src );
-    sr_s sr = bcore_via_p_iget( p, o.cast(c bcore_via* ), index );
+    sr_s sr = bcore_via_p_iget( p, o.cast( c bcore_via* ), index );
     if( sr.is_strong() ) sr.clear();
     return sr;
 };
@@ -248,14 +354,21 @@ func (void selftest() ) =
     ASSERT( inst );
     ASSERT( inst.cast( c u3_t* ).0 == 12345 );
 
-    c x_inst* zoo = cast( bcore_spect_via_create_zoo( 2 ).o, d x_inst* )^;
+    m x_inst* zoo = cast( bcore_spect_via_create_zoo( 2 ).o, d x_inst* )^;
 
     //zoo.to_sink_txt_ml( x_inst_stdout() );
 
     //x_via_path_s!^.parse_sc( "[0]" ).c_get_in( zoo )?.to_sink_txt_ml( x_inst_stdout() );
 
-    sr_s sr = x_via_path_s!^.parse_sc( "[0].[1].vdata.data2.[0]" ).get_sr_in_t( zoo._, zoo );
+    sr_s sr = sr_null();
 
+    sr = x_via_path_s!^.parse_sc( "[0].[1].vdata.data2.[1]" ).get_sr_in( zoo );
+    if( sr.o ) bcore_txt_ml_x_to_sink( sr, x_inst_stdout() );
+
+    sr = x_via_path_s!^.parse_sc( "[0].[1].vdata.data2.[1]" ).set_sr_in( zoo, sr_u3( 20 ) );
+    if( sr.o ) bcore_txt_ml_x_to_sink( sr, x_inst_stdout() );
+
+    sr = x_via_path_s!^.parse_sc( "[0].[1].vdata" ).get_sr_in( zoo );
     if( sr.o ) bcore_txt_ml_x_to_sink( sr, x_inst_stdout() );
 };
 
