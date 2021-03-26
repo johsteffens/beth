f6bc24ab5bf09d4fd41022e809520acc69ed8c0a
johannes <johannes.b.steffens@gmail.com>
2021-03-26 21:44:23 +0100

    un-inlined some functions

--- lib/bcore/bcore_x_inst.h
+++ lib/bcore/bcore_x_inst.h
@@ -27,9 +27,14 @@ XOILA_DEFINE_GROUP( x_inst, bcore_inst )
 
 #ifdef XOILA_SECTION
 
-include "bcore_txt_ml.h";
-include "bcore_bin_ml.h";
-include "bcore_arr.h";
+include 'h' "bcore_txt_ml.h";
+include 'h' "bcore_bin_ml.h";
+include 'h' "bcore_arr.h";
+include 'h' "bcore_life.h";
+include 'h' "bcore_sinks.h";
+include 'h' "bcore_sources.h";
+
+include 'c' "bcore_file.h";
 
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -71,15 +76,34 @@ func (o to_sink_bin_ml(     c@* o, m bcore_sink* sink )) = { bcore_bin_ml_a_to_s
 func (o from_source_txt_ml( m@* o, m bcore_source* source )) = { bcore_txt_ml_a_from_source( o, source ); return o; };
 func (o from_source_bin_ml( m@* o, m bcore_source* source )) = { bcore_bin_ml_a_from_source( o, source ); return o; };
 
+//----------------------------------------------------------------------------------------------------------------------
+
 func (o t_to_sink_txt_ml(     tp_t t, c@* o, m bcore_sink* sink )) = { bcore_txt_ml_t_to_sink( t, o, sink ); return o; };
 func (o t_to_sink_bin_ml(     tp_t t, c@* o, m bcore_sink* sink )) = { bcore_bin_ml_t_to_sink( t, o, sink ); return o; };
 func (o t_from_source_txt_ml( tp_t t, m@* o, m bcore_source* source )) = { bcore_txt_ml_t_from_source( t, o, source ); return o; };
 func (o t_from_source_bin_ml( tp_t t, m@* o, m bcore_source* source )) = { bcore_bin_ml_t_from_source( t, o, source ); return o; };
 
-func (o to_file_txt_ml(   c@* o, sc_t path )) = { return o.to_sink_txt_ml( bcore_file_open_sink( path )^ ); };
-func (o to_file_bin_ml(   c@* o, sc_t path )) = { return o.to_sink_bin_ml( bcore_file_open_sink( path )^ ); };
-func (o from_file_txt_ml( m@* o, sc_t path )) = { return o.from_source_txt_ml( bcore_file_open_source( path )^ ); };
-func (o from_file_bin_ml( m@* o, sc_t path )) = { return o.from_source_bin_ml( bcore_file_open_source( path )^ ); };
+//----------------------------------------------------------------------------------------------------------------------
+
+func (o to_file_txt_ml(   c@* o, sc_t path )) =
+{
+    return o.to_sink_txt_ml( bcore_file_open_sink( path )^ );
+};
+
+func (o to_file_bin_ml(   c@* o, sc_t path )) =
+{
+    return o.to_sink_bin_ml( bcore_file_open_sink( path )^ );
+};
+
+func (o from_file_txt_ml( m@* o, sc_t path )) =
+{
+    return o.from_source_txt_ml( bcore_file_open_source( path )^ );
+};
+
+func (o from_file_bin_ml( m@* o, sc_t path )) =
+{
+    return o.from_source_bin_ml( bcore_file_open_source( path )^ );
+};
 
 //----------------------------------------------------------------------------------------------------------------------
 
