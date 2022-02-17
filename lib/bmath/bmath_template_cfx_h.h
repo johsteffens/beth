c0a156f6ee0e42c1f96244eda64ed05c7a488b6e
johannes <johannes.b.steffens@gmail.com>
2022-02-17 15:42:51 +0100

    simplified code

--- lib/bmath/bmath_template_cfx_h.h
+++ lib/bmath/bmath_template_cfx_h.h
@@ -35,22 +35,31 @@
 static inline
 bmath_cfx_s BCATU(bmath,cfx,init)( fx_t r, fx_t i )
 {
-    return ( bmath_cfx_s ) { .v[0] = r, .v[1] = i };
+    return ( bmath_cfx_s ) { .r = r, .i = i };
+}
+
+/// compact initializer
+static inline
+bmath_cfx_s* BCATU(bmath,cfx,s,_)( bmath_cfx_s* o, fx_t r, fx_t i )
+{
+    o->r = r;
+    o->i = i;
+    return o;
 }
 
 /// init using mag, arg (radius, angle)
 static inline
 bmath_cfx_s BCATU(bmath,cfx,init_mag_arg)( fx_t mag, fx_t arg )
 {
-    return ( bmath_cfx_s ) { .v[0] = mag * cos( arg ), .v[1] = mag * sin( arg ) };
+    return ( bmath_cfx_s ) { .r = mag * cos( arg ), .i = mag * sin( arg ) };
 }
 
 /// init using mag, arg (radius, angle)
 static inline
 bmath_cfx_s* BCATU(bmath,cfx,s,set_mag_arg)( bmath_cfx_s* o, fx_t mag, fx_t arg )
 {
-    o->v[ 0 ] = mag * cos( arg );
-    o->v[ 1 ] = mag * sin( arg );
+    o->r = mag * cos( arg );
+    o->i = mag * sin( arg );
     return o;
 }
 
@@ -59,51 +68,51 @@ static inline
 bmath_cfx_s BCATU(bmath,cfx,init_urt)( s3_t k, s3_t n )
 {
     fx_t arg = ( BCATU(fx,tau)() * k ) / n;
-    return ( bmath_cfx_s ) { .v[0] = cos( arg ), .v[1] = sin( arg ) };
+    return ( bmath_cfx_s ) { .r = cos( arg ), .i = sin( arg ) };
 }
 
 static inline
 bmath_cfx_s BCATU(bmath,cfx,zro)( void )
 {
-    return ( bmath_cfx_s ) { .v[0] = 0, .v[1] = 0 };
+    return ( bmath_cfx_s ) { .r = 0, .i = 0 };
 }
 
 static inline
 bmath_cfx_s BCATU(bmath,cfx,one)( void )
 {
-    return ( bmath_cfx_s ) { .v[0] = 1, .v[1] = 0 };
+    return ( bmath_cfx_s ) { .r = 1, .i = 0 };
 }
 
 static inline
 bl_t BCATU(bmath_cfx_s,is_equ)( const bmath_cfx_s* o, const bmath_cfx_s* op )
 {
-    return ( o->v[ 0 ] == op->v[ 0 ] ) && ( o->v[ 1 ] == op->v[ 1 ] );
+    return ( o->r == op->r ) && ( o->i == op->i );
 }
 
 static inline
 bl_t BCATU(bmath_cfx_s,is_zro)( const bmath_cfx_s* o )
 {
-    return ( o->v[ 0 ] == 0 ) && ( o->v[ 1 ] == 0 );
+    return ( o->r == 0 ) && ( o->i == 0 );
 }
 
 static inline
 bl_t BCATU(bmath_cfx_s,is_one)( const bmath_cfx_s* o )
 {
-    return ( o->v[ 0 ] == 1.0 ) && ( o->v[ 1 ] == 0 );
+    return ( o->r == 1.0 ) && ( o->i == 0 );
 }
 
 static inline
 void BCATU(bmath_cfx_s,add)( const bmath_cfx_s* o, const bmath_cfx_s* op, bmath_cfx_s* res )
 {
-    res->v[ 0 ] = o->v[ 0 ] + op->v[ 0 ];
-    res->v[ 1 ] = o->v[ 1 ] + op->v[ 1 ];
+    res->r = o->r + op->r;
+    res->i = o->i + op->i;
 }
 
 static inline
 void BCATU(bmath_cfx_s,zro)( bmath_cfx_s* o )
 {
-    o->v[ 0 ] = 0;
-    o->v[ 1 ] = 0;
+    o->r = 0;
+    o->i = 0;
 }
 
 /// srt to urt (s. nomenclature); k can be negative indicating the circular direction.
@@ -111,143 +120,143 @@ static inline
 bmath_cfx_s* BCATU(bmath_cfx_s,urt)( bmath_cfx_s* o, s3_t k, s3_t n )
 {
     fx_t arg = ( BCATU(fx,tau)() * k ) / n;
-    o->v[0] = cos( arg );
-    o->v[1] = sin( arg );
+    o->r = cos( arg );
+    o->i = sin( arg );
     return o;
 }
 
 static inline
 void BCATU(bmath_cfx_s,neg)( const bmath_cfx_s* o, bmath_cfx_s* res )
 {
-    res->v[ 0 ] = -o->v[ 0 ];
-    res->v[ 1 ] = -o->v[ 1 ];
+    res->r = -o->r;
+    res->i = -o->i;
 }
 
 static inline
 bmath_cfx_s BCATU(bmath,cfx,neg)( bmath_cfx_s o )
 {
-    return ( bmath_cfx_s ) { .v[0] = -o.v[ 0 ], .v[1] = -o.v[ 1 ] };
+    return ( bmath_cfx_s ) { .r = -o.r, .i = -o.i };
 }
 
 static inline
 void BCATU(bmath_cfx_s,sub)( const bmath_cfx_s* o, const bmath_cfx_s* op, bmath_cfx_s* res )
 {
-    res->v[ 0 ] = o->v[ 0 ] - op->v[ 0 ];
-    res->v[ 1 ] = o->v[ 1 ] - op->v[ 1 ];
+    res->r = o->r - op->r;
+    res->i = o->i - op->i;
 }
 
 static inline
 void BCATU(bmath_cfx_s,mul)( const bmath_cfx_s* o, const bmath_cfx_s* op, bmath_cfx_s* res )
 {
-    fx_t re     = o->v[ 0 ] * op->v[ 0 ] - o->v[ 1 ] * op->v[ 1 ];
-    res->v[ 1 ] = o->v[ 0 ] * op->v[ 1 ] + o->v[ 1 ] * op->v[ 0 ];
-    res->v[ 0 ] = re;
+    fx_t r = o->r * op->r - o->i * op->i;
+    res->i = o->r * op->i + o->i * op->r;
+    res->r = r;
 }
 
 static inline
 bmath_cfx_s BCATU(bmath,cfx,mul)( bmath_cfx_s op1, bmath_cfx_s op2 )
 {
-    fx_t re = op1.v[ 0 ] * op2.v[ 0 ] - op1.v[ 1 ] * op2.v[ 1 ];
-    fx_t im = op1.v[ 0 ] * op2.v[ 1 ] + op1.v[ 1 ] * op2.v[ 0 ];
-    return ( bmath_cfx_s ) { .v[0] = re, .v[1] = im };
+    fx_t re = op1.r * op2.r - op1.i * op2.i;
+    fx_t im = op1.r * op2.i + op1.i * op2.r;
+    return ( bmath_cfx_s ) { .r = re, .i = im };
 }
 
 static inline
 bmath_cfx_s BCATU(bmath,cfx,mul,fx)( bmath_cfx_s op1, fx_t op2 )
 {
-    return ( bmath_cfx_s ) { .v[0] = op1.v[ 0 ] * op2, .v[1] = op1.v[ 1 ] * op2 };
+    return ( bmath_cfx_s ) { .r = op1.r * op2, .i = op1.i * op2 };
 }
 
 static inline
 void BCATU(bmath_cfx_s,sqr)( const bmath_cfx_s* o, bmath_cfx_s* res )
 {
-    fx_t re     = o->v[ 0 ] * o->v[ 0 ] - o->v[ 1 ] * o->v[ 1 ];
-    res->v[ 1 ] = o->v[ 0 ] * o->v[ 1 ] * 2.0;
-    res->v[ 0 ] = re;
+    fx_t re     = o->r * o->r - o->i * o->i;
+    res->i = o->r * o->i * 2.0;
+    res->r = re;
 }
 
 // res = ( o - op )^2
 static inline
 void BCATU(bmath_cfx_s,sub_sqr)( const bmath_cfx_s* o, const bmath_cfx_s* op, bmath_cfx_s* res )
 {
-    fx_t dre  = o->v[ 0 ] - op->v[ 0 ];
-    fx_t dim  = o->v[ 1 ] - op->v[ 1 ];
-    res->v[ 0 ] = dre * dre - dim * dim;
-    res->v[ 1 ] = dre * dim * 2.0;
+    fx_t dre = o->r - op->r;
+    fx_t dim = o->i - op->i;
+    res->r = dre * dre - dim * dim;
+    res->i = dre * dim * 2.0;
 }
 
 // res = o^2 + op
 static inline
 void BCATU(bmath_cfx_s,sqr_add)( const bmath_cfx_s* o, const bmath_cfx_s* op, bmath_cfx_s* res )
 {
-    fx_t re = op->v[0] + o->v[ 0 ] * o->v[ 0 ] - o->v[ 1 ] * o->v[ 1 ];
-    fx_t im = op->v[1] + o->v[ 0 ] * o->v[ 1 ] * 2.0;
-    res->v[ 0 ] = re;
-    res->v[ 1 ] = im;
+    fx_t re = op->r + o->r * o->r - o->i * o->i;
+    fx_t im = op->i + o->r * o->i * 2.0;
+    res->r = re;
+    res->i = im;
 }
 
 static inline
 void BCATU(bmath_cfx_s,mul,fx)( const bmath_cfx_s* o, fx_t op, bmath_cfx_s* res )
 {
-    res->v[ 0 ] = o->v[ 0 ] * op;
-    res->v[ 1 ] = o->v[ 1 ] * op;
+    res->r = o->r * op;
+    res->i = o->i * op;
 }
 
 // res = o + ( op1 * op2 )
 static inline
 void BCATU(bmath_cfx_s,add_prod)( const bmath_cfx_s* o, const bmath_cfx_s* op1, const bmath_cfx_s* op2, bmath_cfx_s* res )
 {
-    fx_t re     = o->v[ 0 ] + ( op1->v[ 0 ] * op2->v[ 0 ] - op1->v[ 1 ] * op2->v[ 1 ] );
-    res->v[ 1 ] = o->v[ 1 ] + ( op1->v[ 0 ] * op2->v[ 1 ] + op1->v[ 1 ] * op2->v[ 0 ] );
-    res->v[ 0 ] = re;
+    fx_t re = o->r + ( op1->r * op2->r - op1->i * op2->i );
+    res->i = o->i + ( op1->r * op2->i + op1->i * op2->r );
+    res->r = re;
 }
 
 // res = o - ( op1 * op2 )
 static inline
 void BCATU(bmath_cfx_s,sub_prod)( const bmath_cfx_s* o, const bmath_cfx_s* op1, const bmath_cfx_s* op2, bmath_cfx_s* res )
 {
-    fx_t re     = o->v[ 0 ] - ( op1->v[ 0 ] * op2->v[ 0 ] - op1->v[ 1 ] * op2->v[ 1 ] );
-    res->v[ 1 ] = o->v[ 1 ] - ( op1->v[ 0 ] * op2->v[ 1 ] + op1->v[ 1 ] * op2->v[ 0 ] );
-    res->v[ 0 ] = re;
+    fx_t re = o->r - ( op1->r * op2->r - op1->i * op2->i );
+    res->i = o->i - ( op1->r * op2->i + op1->i * op2->r );
+    res->r = re;
 }
 
 // res = o * ( op1 - op2 )
 static inline
 void BCATU(bmath_cfx_s,mul_diff)( const bmath_cfx_s* o, const bmath_cfx_s* op1, const bmath_cfx_s* op2, bmath_cfx_s* res )
 {
-    fx_t dre = op1->v[ 0 ] - op2->v[ 0 ];
-    fx_t dim = op1->v[ 1 ] - op2->v[ 1 ];
+    fx_t dre = op1->r - op2->r;
+    fx_t dim = op1->i - op2->i;
 
-    fx_t re     = o->v[ 0 ] * dre - o->v[ 1 ] * dim;
-    res->v[ 1 ] = o->v[ 0 ] * dim + o->v[ 1 ] * dre;
-    res->v[ 0 ] = re;
+    fx_t re = o->r * dre - o->i * dim;
+    res->i = o->r * dim + o->i * dre;
+    res->r = re;
 }
 
 static inline
 void BCATU(bmath_cfx_s,one)( bmath_cfx_s* o )
 {
-    o->v[ 0 ] = 1;
-    o->v[ 1 ] = 0;
+    o->r = 1;
+    o->i = 0;
 }
 
 static inline
 fx_t BCATU(bmath,cfx,det)( bmath_cfx_s o )
 {
-    return BCATU(fx,sqr)( o.v[ 0 ] ) + BCATU(fx,sqr)( o.v[ 1 ] );
+    return BCATU(fx,sqr)( o.r ) + BCATU(fx,sqr)( o.i );
 }
 
 /// total sum of squares (same as det)
 static inline
 fx_t BCATU(bmath,cfx,tss)( bmath_cfx_s o )
 {
-    return BCATU(fx,sqr)( o.v[ 0 ] ) + BCATU(fx,sqr)( o.v[ 1 ] );
+    return BCATU(fx,sqr)( o.r ) + BCATU(fx,sqr)( o.i );
 }
 
 /// total sum of squares (same as det)
 static inline
 fx_t BCATU(bmath,cfx,s,tss)( const bmath_cfx_s* o )
 {
-    return BCATU(fx,sqr)( o->v[ 0 ] ) + BCATU(fx,sqr)( o->v[ 1 ] );
+    return BCATU(fx,sqr)( o->r ) + BCATU(fx,sqr)( o->i );
 }
 
 /// magnitude (srt(tss))
@@ -261,37 +270,37 @@ fx_t BCATU(bmath,cfx,s,mag)( const bmath_cfx_s* o )
 static inline
 fx_t BCATU(bmath,cfx,s,arg)( const bmath_cfx_s* o )
 {
-    return ( o->v[ 0 ] != 0 || o->v[ 1 ] != 0 ) ? atan2( o->v[ 1 ], o->v[ 0 ] ) : 0;
+    return ( o->r != 0 || o->i != 0 ) ? atan2( o->i, o->r ) : 0;
 }
 
 static inline
 void BCATU(bmath_cfx_s,inv)( const bmath_cfx_s* o, bmath_cfx_s* res )
 {
     fx_t inv = BCATU(fx,inv)( BCATU(bmath,cfx,det)( *o ) );
-    res->v[ 0 ] =   o->v[ 0 ] * inv;
-    res->v[ 1 ] = - o->v[ 1 ] * inv;
+    res->r =   o->r * inv;
+    res->i = - o->i * inv;
 }
 
 static inline
 void BCATU(bmath_cfx_s,div)( const bmath_cfx_s* o, const bmath_cfx_s* op, bmath_cfx_s* res )
 {
     fx_t inv = BCATU(fx,inv)( BCATU(bmath,cfx,det)( *op ) );
-    fx_t re     = ( o->v[ 0 ] * op->v[ 0 ] + o->v[ 1 ] * op->v[ 1 ] ) * inv;
-    res->v[ 1 ] = ( o->v[ 1 ] * op->v[ 0 ] - o->v[ 0 ] * op->v[ 1 ] ) * inv;
-    res->v[ 0 ] = re;
+    fx_t re = ( o->r * op->r + o->i * op->i ) * inv;
+    res->i = ( o->i * op->r - o->r * op->i ) * inv;
+    res->r = re;
 }
 
 static inline
 void BCATU(bmath_cfx_s,cnj)( const bmath_cfx_s* o, bmath_cfx_s* res )
 {
-    res->v[ 0 ] =  o->v[ 0 ];
-    res->v[ 1 ] = -o->v[ 1 ];
+    res->r =  o->r;
+    res->i = -o->i;
 }
 
 static inline
 void BCATU(bmath_cfx_s,self_cnj)( bmath_cfx_s* o )
 {
-    o->v[ 1 ] = -o->v[ 1 ];
+    o->i = -o->i;
 }
 
 static inline
@@ -311,13 +320,13 @@ fx_t BCATU(bmath,cfx,diff_mag)( bmath_cfx_s o, bmath_cfx_s b )
 static inline
 fx_t BCATU(bmath,cfx,arg)( bmath_cfx_s o )
 {
-    return ( o.v[ 0 ] != 0 || o.v[ 1 ] != 0 ) ? atan2( o.v[ 1 ], o.v[ 0 ] ) : 0;
+    return ( o.r != 0 || o.i != 0 ) ? atan2( o.i, o.r ) : 0;
 }
 
 static inline
 bl_t BCATU(bmath,cfx,equ)( bmath_cfx_s o, bmath_cfx_s op1 )
 {
-    return o.v[ 0 ] == op1.v[ 0 ] && o.v[ 1 ] == op1.v[ 1 ];
+    return o.r == op1.r && o.i == op1.i;
 }
 
 /// For easy inspection
