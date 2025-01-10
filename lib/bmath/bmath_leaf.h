8b269e59ceffbba323212a6db94f96ba5137f834
Johannes Steffens <johannes.b.steffens@gmail.com>
2025-01-10 18:08:58 +0100

    moved some leaf functions to bcore

--- lib/bmath/bmath_leaf.h
+++ lib/bmath/bmath_leaf.h
@@ -28,9 +28,10 @@
 
 u2_t bmath_u2_argb_from_f3( vd_t, f3_t v ); // fist argument ignored (can be NULL)
 
-typedef f3_t (*bmath_fp_f3_ar0 )( void           ); // nullary function   -> f3
-typedef f3_t (*bmath_fp_f3_ar1 )( f3_t a         ); // unary function  f3 -> f3
-typedef f3_t (*bmath_fp_f3_ar2 )( f3_t a, f3_t b ); // binary function (f3,f3) -> f3
+typedef f3_t (*bmath_fp_f3_ar0 )( void                   ); // nullary function   -> f3
+typedef f3_t (*bmath_fp_f3_ar1 )( f3_t a                 ); // unary function  f3 -> f3
+typedef f3_t (*bmath_fp_f3_ar2 )( f3_t a, f3_t b         ); // binary function (f3,f3) -> f3
+typedef f3_t (*bmath_fp_f3_ar3 )( f3_t a, f3_t b, f3_t c ); // ternary function (f3,f3) -> f3
 
 typedef f3_t (*bmath_fp_f3_op_ar0 )( vc_t op );                 // arity 0 operator
 typedef f3_t (*bmath_fp_f3_op_ar1 )( vc_t op, f3_t a );         // arity 1 operator
@@ -41,9 +42,10 @@ typedef f3_t (*bmath_fp_f3_op_ar2 )( vc_t op, f3_t a, f3_t b ); // arity 2 opera
 
 u2_t bmath_u2_argb_from_f2( vd_t, f2_t v ); // fist argument ignored (can be NULL)
 
-typedef f2_t (*bmath_fp_f2_ar0 )( void           ); // nullary function   -> f2
-typedef f2_t (*bmath_fp_f2_ar1 )( f2_t a         ); // unary function  f2 -> f2
-typedef f2_t (*bmath_fp_f2_ar2 )( f2_t a, f2_t b ); // binary function (f2,f2) -> f2
+typedef f2_t (*bmath_fp_f2_ar0 )( void                   ); // nullary function   -> f2
+typedef f2_t (*bmath_fp_f2_ar1 )( f2_t a                 ); // unary function  f2 -> f2
+typedef f2_t (*bmath_fp_f2_ar2 )( f2_t a, f2_t b         ); // binary function (f2,f2) -> f2
+typedef f2_t (*bmath_fp_f2_ar3 )( f2_t a, f2_t b, f2_t c ); // ternary function (f2,f2,f2) -> f2
 
 typedef f2_t (*bmath_fp_f2_op_ar0 )( vc_t op );                 // arity 0 operator
 typedef f2_t (*bmath_fp_f2_op_ar1 )( vc_t op, f2_t a );         // arity 1 operator
